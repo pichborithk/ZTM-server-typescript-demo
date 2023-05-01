6a1a617f3312481d93857f797848312ed34c2e82
Pichborith Kong <pichborith@gmail.com>
2023-05-01 16:29:41 -0700

    add router and bodyparser middleware

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000');
+});
